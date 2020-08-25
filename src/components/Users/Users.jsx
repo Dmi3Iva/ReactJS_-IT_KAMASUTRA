a250a69a4518d40d0c89c2c50a1615000069cc0d
Dmitriy Ivanov <Dmi3iva@gmail.com>
2020-08-25 23:03:51 +0200

    lesson 52. Clear user component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,26 +3,35 @@ import styles from './users.module.css';
 import * as axios from "axios";
 import defaultUserPhoto from '../assets/freepik-avatar.jpg';
 
-const Users = (props)=>{
-    if(props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response =>{
-                props.setUsers(response.data.items);
-            });
+const Users = (props) => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
 
     return (<div>
+        <button onClick={getUsers}>Get Users</button>
         {
-            props.users.map(x=> <div key={x.id}>
+            props.users.map(x => <div key={x.id}>
                 <span>
                     <div>
-                        <img className={styles.userPhoto} src={x.photos.small != null ? x.photos.small : defaultUserPhoto} alt={`image of ${x.name}`} />
+                        <img className={styles.userPhoto}
+                             src={x.photos.small != null ? x.photos.small : defaultUserPhoto}
+                             alt={`image of ${x.name}`}/>
                     </div>
                     <div>
                         {x.followed ?
-                            <button onClick={()=>{props.unfollow(x.id)}}>Follow</button>
+                            <button onClick={() => {
+                                props.unfollow(x.id)
+                            }}>Follow</button>
                             :
-                            <button onClick={()=>{props.follow(x.id)}}>Unfollow</button>
+                            <button onClick={() => {
+                                props.follow(x.id)
+                            }}>Unfollow</button>
                         }
                     </div>
                 </span>
