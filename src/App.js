a2035a278c71529e9a3281b388b27074d8827847
Dmitriy Ivanov <Dmi3iva@gmail.com>
2020-08-27 22:29:04 +0200

    lesson 61. add auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Header from './components/Header/Header.jsx';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import DialogsContainer from './components/Dialogs/DialogsContainer.jsx';
 import styles from './App.module.css';
 import {Route} from 'react-router-dom';
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
   return (
 	  <div>
 		<div className={`${styles.App} ${styles.wrapper}`}>
-			<Header />
+			<HeaderContainer />
 			<Sidebar />
 			<div className={styles.content}>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-function Header(){
+function Header(props){
+	debugger;
 	return (
 		<header className={styles.header}>
 			<img className={styles.header__logo} src={logo} alt="Logo"/>
+			<div className={styles.login__block}>
+				{props.isAuth ?
+					<div>{props.login}</div>
+					:<NavLink to={'/login'}>Login</NavLink>
+				}
+			</div>
 		</header>
 	);
 }
