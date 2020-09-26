ac4fb173b9e8409e4de309954a3d99f007783302
Dmitriy Ivanov <Dmi3iva@gmail.com>
2020-09-26 19:51:45 +0200

    BROKEN, add switch to profile page

--- src/components/Profile/Description/ProfileData/ProfileData.jsx
+++ src/components/Profile/Description/ProfileData/ProfileData.jsx
@@ -1,37 +1,69 @@
-import style from "../Description.module.css";
 import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import FaceBookIcon from "@material-ui/icons/Facebook";
+import VkIcon from './vk.svg';
+import {SvgIcon} from "@material-ui/core";
+
+const styles = theme => ({
+    contacts: {
+        display: 'flex',
+        flexWrap: 'wrap',
+
+        '& > a': {
+
+        }
+    }
+});
 
 const ProfileData = (props) => {
+    const {classes} = props;
+
+    const renderSocials = (c, cVal) => {
+        const wrapInLink = (Component, href) => {
+            return <a href={href}><Component/></a>;
+        }
+
+        switch (c.toString().toLowerCase()) {
+            case 'facebook': {
+                return wrapInLink(FaceBookIcon, cVal);
+            }
+            case 'vk': {
+                return wrapInLink(()=>(<SvgIcon component={VkIcon}/>), cVal);
+            }
+            default: {
+                return wrapInLink(()=>(<div>{c}</div>), cVal);
+            }
+        }
+    }
+
     return (
         <div>
-            {props.isOwner && <button onClick={props.turnOnEditMode}>Edit</button> }
             <div className="fullName">{props.profile.fullName}</div>
             <div className="aboutMe">{props.profile.aboutMe}</div>
-
             <div>
                 {props.profile.lookingForAJob && props.profile.lookingForAJobDescription}
             </div>
             <div>
                 {(props.profile.contacts.length !== 0) && "Социальные сети"}
             </div>
-            <dl className={style.contacts}>
+            <dl className={classes.contacts}>
                 {
                     (props.profile.contacts.length !== 0) &&
                     Object.keys(props.profile.contacts).map(c =>
                         (
-                            <div key={c}>
-                                <dt className={"contacts_item"}>
-                                    {c}
-                                </dt>
-                                <dd>
-                                    {props.profile.contacts[c]}
-                                </dd>
-                            </div>
+                            <React.Fragment key={c}>
+                                {renderSocials(c, props.profile.contacts[c])}
+                            </ React.Fragment>
                         )
                     )
                 }
             </dl>
+            {
+                props.isOwner &&
+                <Button color={'primary'} variant={'contained'} onClick={props.turnOnEditMode}>Edit my profile</Button>
+            }
         </div>);
 }
 
-export default ProfileData;
+export default withStyles(styles)(ProfileData);
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Avatar from './Avatar/Avatar.jsx';
-import MyPostsContainer from './MyPosts/MyPostsContainer.jsx';
 import styles from './Profile.module.css';
-import WriteNewPostContainer from "./WriteNewPost/WriteNewPostContainer";
 import Preloader from "../Common/Preloader/Preloader";
 import DescriptionContainer from "./Description/DescriptionContainer";
+{/*
+import MyPostsContainer from './MyPosts/MyPostsContainer.jsx';
+import WriteNewPostContainer from "./WriteNewPost/WriteNewPostContainer";
+*/}
 
 const  Profile = (props) => {
 	if(!props.profile) {
@@ -15,8 +17,10 @@ const  Profile = (props) => {
 		<div className={styles.profile}>
 			<Avatar photos={props.profile.photos} isOwner={props.isOwner}/>
 			<DescriptionContainer profile={props.profile} status={props.status} isOwner={props.isOwner}/>
+			{/*
 			<WriteNewPostContainer />
 			<MyPostsContainer />
+			*/}
 		</div>
 	);
 }
